2a950ed8a3db84f60168fc61fc233a70bc30b78a
Savranin Denis <savranindenisv@gmail.com>
2024-12-04 15:12:51 +0200

    added adaptation header

--- js/index.js
+++ js/index.js
@@ -27,3 +27,22 @@
     refs.modal.classList.toggle('is-open');
   }
 })();
+
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openMenuBtn: document.querySelector('[menu-mobile-open]'),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeMenuBtn: document.querySelector('[menu-mobile-close]'),
+    // Додати атрибут data-modal на бекдроп модалки
+    menubar: document.querySelector('[menu-mobilel]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.menubar.classList.toggle('is-open');
+  }
+})();
